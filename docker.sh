1b8c5ad4722a84785c894f1cd36b4438b29bbaf2
Danger <144523105+DangerDrome@users.noreply.github.com>
2023-11-07 12:44:52 +1300

    Create docker.sh

---
+++ docker.sh
@@ -0,0 +1,89 @@
+#!/usr/bin/env bash
+source <(curl -s https://raw.githubusercontent.com/tteck/Proxmox/main/misc/build.func)
+# Copyright (c) 2021-2023 tteck
+# Author: tteck (tteckster)
+# License: MIT
+# https://github.com/tteck/Proxmox/raw/main/LICENSE
+
+function header_info {
+clear
+cat <<"EOF"
+                                                                      DANGERDD#
+                                                                  DANGERDANGERDD
+                                                        DANGERDD DANGER    DDDD
+                                                DANGER  DDD      %DDDD    DDDD
+                                            DANGER  *DD DDD%DANGERDDDD   DDDDD
+                              %DDD    DD.  #DDDDD     DANGER*    DDDDD DDDDD
+              DANGERDDDDD    DDDD    DDD DANGER      .DDD.   DDDDANGERDDD
+            DANGERDANGERDDD  DD  DDD DDDD/DDDDD% DANGERDANGERDANGERDANGER
+        #DANGERDD(    DDDDD(DANGERDDDDD DANGERD   DDD#DD       DDDD /DDDDD
+        DANGERDD      *DDDDANGERDDD,DD  DDD.DDD,DDDD          DDDDD   DANGER
+          DDDD.      DANGERD    DDDDD  DDD  DDDD             DDDD      DDDDD
+          DDDDD       DANGER     DDDD   .D                   /DDD%       DANGER
+        DDDDD       DANGER/      DDD                        DDDD          DANGER
+      DANGERD        DDDDD*       D                         DDD%             DANGER
+      DDDDD       DANGER#                                                     DDDDD
+      DDDD       DANGER                                                         DDDD
+      DDDD     DANGERD
+    DDDDD  %DANGERD
+    DANGERDANGERD
+    DANGERDDD
+.DDDDD# .D
+  DD____             __            
+   / __ \____  _____/ /_  __  _____
+  / / / / __ \/ ___/ //_/ _ \/ ___/
+ / /_/ / /_/ / /__/ ,< /  __/ /    
+/_____/\____/\___/_/|_|\___/_/     
+ 
+EOF
+}
+header_info
+echo -e "Loading..."
+APP="Docker"
+var_disk="128"
+var_cpu="2"
+var_ram="2048"
+var_os="debian"
+var_version="12"
+variables
+color
+catch_errors
+
+function default_settings() {
+  CT_TYPE="1"
+  PW=""
+  CT_ID=$NEXTID
+  HN=$NSAPP
+  DISK_SIZE="$var_disk"
+  CORE_COUNT="$var_cpu"
+  RAM_SIZE="$var_ram"
+  BRG="vmbr0"
+  NET="dhcp"
+  GATE=""
+  DISABLEIP6="no"
+  MTU=""
+  SD=""
+  NS=""
+  MAC=""
+  VLAN=""
+  SSH="no"
+  FUSE="no"
+  VERB="no"
+  echo_default
+}
+
+function update_script() {
+header_info
+if [[ ! -d /var ]]; then msg_error "No ${APP} Installation Found!"; exit; fi
+msg_info "Updating ${APP} LXC"
+apt-get update &>/dev/null
+apt-get -y upgrade &>/dev/null
+msg_ok "Updated ${APP} LXC"
+exit
+}
+
+start
+build_container
+description
+
+msg_ok "Completed Successfully!\n"
